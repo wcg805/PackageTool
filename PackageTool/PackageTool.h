cc32fb1f1d56f50991f2e156f001192a9d51682b
Miles <Miles@MILES-PC>
2017-02-04 23:34:07 +0800

    添加项目文件。

---
+++ PackageTool/PackageTool.h
@@ -0,0 +1,32 @@
+
+// PackageTool.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CPackageToolApp: 
+// 有关此类的实现，请参阅 PackageTool.cpp
+//
+
+class CPackageToolApp : public CWinApp
+{
+public:
+	CPackageToolApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CPackageToolApp theApp;
